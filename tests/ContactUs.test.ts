650f460241cf0a934476b8f3aae75c55f11a5741
Inna8888 <103404310+Inna8888@users.noreply.github.com>
2024-03-03 12:16:05 +0200

    init

---
+++ tests/ContactUs.test.ts
@@ -0,0 +1,13 @@
+import { randomUUID } from 'crypto';
+import { test, expect } from '@playwright/test';
+
+test('user can submit contact us form', async ({ page }) => {
+  await page.goto('https://shopdemo-alex-hot.koyeb.app/');
+  await page.getByRole ('link', {name: 'Contact Us'}).first().click();
+  await page.getByPlaceholder('You Full Name').fill('test');
+  await page.getByPlaceholder('Your Email Address').fill(`test.${randomUUID}@gmail.com`);
+  await page.getByPlaceholder('Please Describe Your Message').fill('test hfjhsdkfd gfhfjh hghjghjhgjghjg');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(page.getByPlaceholder('Please Describe Your message')).toBeEmpty();
+});
+
