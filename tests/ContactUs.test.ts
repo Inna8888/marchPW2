29d5ca8014e01298e7142a680d7c4678b5e2e758
Inna8888 <103404310+Inna8888@users.noreply.github.com>
2024-03-03 12:29:13 +0200

    fixed CI

--- tests/ContactUs.test.ts
+++ tests/ContactUs.test.ts
@@ -5,7 +5,7 @@ test('user can submit contact us form', async ({ page }) => {
   await page.goto('https://shopdemo-alex-hot.koyeb.app/');
   await page.getByRole ('link', {name: 'Contact Us'}).first().click();
   await page.getByPlaceholder('You Full Name').fill('test');
-  await page.getByPlaceholder('Your Email Address').fill(`test.${randomUUID}@gmail.com`);
+  await page.getByPlaceholder('Your Email Address').fill(`test.${ randomUUID }test@gmail.com`);
   await page.getByPlaceholder('Please Describe Your Message').fill('test hfjhsdkfd gfhfjh hghjghjhgjghjg');
   await page.getByRole('button', { name: 'Submit' }).click();
   await expect(page.getByPlaceholder('Please Describe Your message')).toBeEmpty();
